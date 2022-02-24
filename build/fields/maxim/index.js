f583516333a9b5702bc4ddcf7770a3184ebf164a
Yuta Ohira <alesion90@gmail.com>
2022-02-25 02:57:45 +0900

    :fire: ビルドを削除

--- build/fields/maxim/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.maximField = void 0;
-const query_1 = require("./query");
-/** Maxim 名言 */
-exports.maximField = {
-    query: query_1.maximQuery,
-};
--- build/functions/getRandom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandom = void 0;
-/** 乱数を返す */
-const getRandom = (min, max) => {
-    const random = Math.floor(Math.random() * (max + 1 - min)) + min;
-    return random;
-};
-exports.getRandom = getRandom;
