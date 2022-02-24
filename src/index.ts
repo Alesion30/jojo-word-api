0566d882e0b54405597134ae291ecfe12dd7cd5e
Yuta Ohira <alesion90@gmail.com>
2022-02-24 22:39:41 +0900

    :sparkles: graphqlを導入

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('hello > jojo word api')
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { buildSchema } from 'graphql'
+
+const schema = buildSchema(`
+ type Query {
+ message: String
+ }
+`)
+const root = {
+  message: () => 'Hello World!',
+}
+
+const app = express()
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
+app.listen(4000, () =>
+  console.log('Express GraphQL Server Now Running On localhost:4000/graphql')
+)
