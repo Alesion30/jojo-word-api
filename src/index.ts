7be1996137a55db3a5c2a6c80449c450d8f46575
Yuta Ohira <alesion90@gmail.com>
2022-02-25 23:50:27 +0900

    :hankey: eslint-disable-next-line no-console

--- src/index.ts
+++ src/index.ts
@@ -29,5 +29,6 @@ app.use(
 // Run Server
 const port = process.env.PORT || 4000
 app.listen(port, () =>
+  // eslint-disable-next-line no-console
   console.log(`Express GraphQL Server Now Running On localhost:${port}/graphql`)
 )
